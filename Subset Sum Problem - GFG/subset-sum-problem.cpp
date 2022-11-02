63a28d8731670bcc5f6bcb1e176e7ca2cddc11d9
Utkarsh Goyal <utkarsh2010202@akgec.ac.in>
2022-11-02 21:53:08 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,28 +9,44 @@ using namespace std;
 
 class Solution{   
 public:
-    bool f(int ind,int sum,vector<int> &arr,vector<vector<int>> &dp)
+    bool f(int ind,int sum,vector<int> arr,vector<vector<int>> dp)
     {
         if(sum==0)
-        return true;
+        return 1;
         if(ind==0)
         return (arr[0]==sum);
         
         if(dp[ind][sum]!=-1)
         return dp[ind][sum];
         
-        int nottake=f(ind-1,sum,arr,dp);
-        int take=false;
+        bool nt=f(ind-1,sum,arr,dp);
+        bool t=0;
         if(arr[ind]<=sum)
-        take=f(ind-1,sum-arr[ind],arr,dp);
+        t=f(ind-1,sum-arr[ind],arr,dp);
         
-        return dp[ind][sum]=take | nottake;
+        return t|nt;
     }
-
     bool isSubsetSum(vector<int>arr, int sum){
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return f(n-1,sum,arr,dp);
+        vector<vector<int>> dp(n,vector<int>(sum+1,0));
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=1;
+        }
+        dp[0][arr[0]]=1;
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int tar=1;tar<sum+1;tar++)
+            {
+                bool nt=dp[ind-1][tar];
+                bool t=0;
+                if(arr[ind]<=tar)
+                t=dp[ind-1][tar-arr[ind]];
+                
+                dp[ind][tar]=t|nt;
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 
