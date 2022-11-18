32b49ac0f88e195e7bea02a11965c1f0ab3ee9e0
Utkarsh Goyal <utkarsh2010202@akgec.ac.in>
2022-11-18 20:18:15 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -20,10 +20,30 @@ class Solution {
         
         return dp[i][j]=1+min(f(i-1,j-1,s1,s2,dp),min(f(i,j-1,s1,s2,dp),f(i-1,j,s1,s2,dp)));
     }
-    int editDistance(string s, string t) {
-        int n=s.size(),m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
+    int editDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=i;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                dp[i][j]=dp[i-1][j-1];
+                else
+                dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+            }
+        }
+        return dp[n][m];
     }
 };
 
