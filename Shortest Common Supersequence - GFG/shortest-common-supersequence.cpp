e080e8ab3ab60d76a4099792812398da47a52584
Utkarsh Goyal <utkarsh2010202@akgec.ac.in>
2022-11-18 19:26:57 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -28,41 +28,41 @@ class Solution
             }
         }
         
-        int len=dp[n][m];
-        int i=n,j=m;
-        string ans="";
+        // int len=dp[n][m];
+        // int i=n,j=m;
+        // string ans="";
         
-        while(i>0&&j>0)
-        {
-            if(s1[i-1]==s2[j-1])
-            {
-                ans+=s1[i-1];
-                i--,j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1])
-            {
-                ans+=s1[i-1];
-                i--;
-            }
-            else
-            {
-                ans+=s2[j-1];
-                j--;
-            }
-        }
+        // while(i>0&&j>0)
+        // {
+        //     if(s1[i-1]==s2[j-1])
+        //     {
+        //         ans+=s1[i-1];
+        //         i--,j--;
+        //     }
+        //     else if(dp[i-1][j]>dp[i][j-1])
+        //     {
+        //         ans+=s1[i-1];
+        //         i--;
+        //     }
+        //     else
+        //     {
+        //         ans+=s2[j-1];
+        //         j--;
+        //     }
+        // }
         
-        while(i>0)
-        {
-            ans+=s1[i-1];
-            i--;
-        }
-        while(j>0)
-        {
-            ans+=s2[j-1];
-            j--;
-        }
+        // while(i>0)
+        // {
+        //     ans+=s1[i-1];
+        //     i--;
+        // }
+        // while(j>0)
+        // {
+        //     ans+=s2[j-1];
+        //     j--;
+        // }
         
-        return ans.length();
+        return n+m-(dp[n][m]);
         
     }
 };
