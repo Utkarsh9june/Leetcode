0d2a20b8760aba10804357d0446aaa03a7bd46e2
Utkarsh Goyal <utkarsh2010202@akgec.ac.in>
2022-12-29 18:56:33 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -14,7 +14,7 @@ class Solution {
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) 
     {
-        vector<int> dist(V,1e9);
+        vector<int> dist(V,1e8);
         dist[S]=0;
         for(int i=0;i<V;i++)
         {
@@ -23,7 +23,7 @@ class Solution {
                 int u=it[0];
                 int v=it[1];
                 int w=it[2];
-                if(dist[u]+w<dist[v] && dist[u]!=1e9)
+                if(dist[u]+w<dist[v])
                 {
                     dist[v]=dist[u]+w;
                 }
